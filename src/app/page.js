28b074db7490189a26359920cf5a48d36a76996e
Suraj Pandit <suraejpaedt@gmail.com>
2025-05-31 22:24:55 +0545

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="w-full">
+      <div className="container ">
+        Hello, container should be centered now.
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1240px",
+        "2xl": "1525px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
